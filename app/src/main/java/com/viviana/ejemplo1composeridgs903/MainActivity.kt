bc663986f7e6a6655a5450ca75f2a489a0b4273e
IDGS-803-22001758 <79677@alumnos.utleon.edu.mx>
2025-06-12 22:58:34 -0600

    Personajes terminados 12/06/2025

--- app/src/main/java/com/viviana/ejemplo1composeridgs903/MainActivity.kt
+++ app/src/main/java/com/viviana/ejemplo1composeridgs903/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.annotation.ColorInt
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
@@ -27,9 +28,14 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
@@ -38,11 +44,11 @@ import androidx.compose.ui.unit.dp
 
 private val tarjetas:List<PersonajeTarjeta> = listOf(
     PersonajeTarjeta("Goku","El protagonista de la serie, conocido por su gran poder y personalidad amigable. Originalmente enviado a la Tierra como un infante volador con la misión de conquistarla."),
-    PersonajeTarjeta("Master Roshi","Maestro de artes marciales y mentor de Goku. Conocido bajo el nombre de Muten Rosh, fue en su momento el terrícola más fuerte de la Tierra, y mucha gente lo recuerda como el \"Dios de las Artes Marciales\", pero antes de poder ostentar dicho título tuvo que trabajar y estudiar las artes marciales con su maestro Mutaito y su eterno rival Tsuru-Sen'nin."),
+    PersonajeTarjeta("Masterroshi","Maestro de artes marciales y mentor de Goku. Conocido bajo el nombre de Muten Rosh, fue en su momento el terrícola más fuerte de la Tierra, y mucha gente lo recuerda como el \"Dios de las Artes Marciales\", pero antes de poder ostentar dicho título tuvo que trabajar y estudiar las artes marciales con su maestro Mutaito y su eterno rival Tsuru-Sen'nin."),
     PersonajeTarjeta("Gohan","Son Gohanda en su tiempo en España, o simplemente Gohan en Hispanoamérica, es uno de los personajes principales de los arcos argumentales de Dragon Ball Z, Dragon Ball Super y Dragon Ball GT. Es un mestizo entre saiyano y humano terrícola."),
     PersonajeTarjeta("Whis","Whis es uno de los hijos del Gran Sacerdote, hermano menor de Vados, Korn y Kus. Es el ángel guía encargado de asistir y servir como maestro al Dios de la Destrucción del Universo 7, Beerus."),
     PersonajeTarjeta("Bills","Dios de la Destrucción Beerus, conocido también como Beers, o Bills en Hispanoamérica e inicialmente en España[1], es un personaje que fue introducido en la película Dragon Ball Z: La batalla de los dioses, donde es el antagonista principal de la película, y que aparece en el manga y anime de Dragon Ball Super como un personaje principal."),
-    PersonajeTarjeta("Kaio del norte (Kaito)","Se trata del dios encargado de administrar las Galaxias del Norte, el cuadrante boreal del Universo 7 y supervisar a los dioses de los planetas de dicho sector (como es el caso de Kami en la Tierra)"),
+    PersonajeTarjeta("Kaio","Se trata del dios encargado de administrar las Galaxias del Norte, el cuadrante boreal del Universo 7 y supervisar a los dioses de los planetas de dicho sector (como es el caso de Kami en la Tierra)"),
     PersonajeTarjeta("Trunks","Hijo de Vegeta y Bulma. Es un mestizo entre humano terrícola y Saiyano nacido en la Tierra, e hijo de Bulma y Vegeta, el cual es introducido en el Arco de los Androides y Cell."),
     PersonajeTarjeta("Piccolo","Es un namekiano que surgió tras ser creado en los últimos momentos de vida de su padre, siendo su actual reencarnación. Aunque en un principio fue el archienemigo de Son Goku, con el paso del tiempo fue haciéndose menos malvado hasta finalmente convertirse en un ser bondadoso y miembro de los Guerreros Z."),
     PersonajeTarjeta("Dende","Sucesor de Piccolo como el nuevo Namekian protector de la Tierra. Es un pequeño namekiano, que vivía en el poblado de Moori, junto a su hermano Scargo y otros tantos de su especie. Es el hijo 108 del Gran Patriarca y posteriormente Dios de la Tierra, sustituyendo a Dios."),
@@ -67,24 +73,36 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun Personaje(name:String, color: Color,style:TextStyle) {
-    Text(text = name)
+fun Personaje(name:String, color: Color,style:TextStyle, lines: Int=Int.MAX_VALUE) {
+    Text(text = name, color = color, style = style, maxLines = lines)
 
 }
 
 @Composable
-fun Personajes(personaje: PersonajeTarjeta){
-    Column {
-        Personaje(personaje.title,
-            MaterialTheme.colorScheme.primary,
-            MaterialTheme.typography.headlineMedium)
+fun Personajes(personaje: PersonajeTarjeta) {
+    var expanded by remember { mutableStateOf(false) }
+    Column(
+        modifier = Modifier.padding(start = 8.dp).clickable {
+            expanded = !expanded
+        }
+    )
+    {
+        Column {
+            Personaje(
+                personaje.title,
+                MaterialTheme.colorScheme.primary,
+                MaterialTheme.typography.headlineMedium
+            )
 
-        Personaje(personaje.body,
-            MaterialTheme.colorScheme.onBackground,
-            MaterialTheme.typography.bodyLarge)
+            Personaje(
+                personaje.body,
+                MaterialTheme.colorScheme.onBackground,
+                MaterialTheme.typography.bodyLarge,
+                if(expanded) Int.MAX_VALUE else 1
+            )
+        }
     }
 }
-
 @Composable
 fun Tarjeta(personajes: List<PersonajeTarjeta>){
     LazyColumn {
@@ -110,16 +128,20 @@ fun MyPersonajes(personaje: PersonajeTarjeta){
       modifier = Modifier.padding(8.dp)
           .background(MaterialTheme.colorScheme.background)
     ) {
-        ImagenHeroe()
+        ImagenHeroe(personaje.title)
         Personajes(personaje)
     }
 
 }
 
 @Composable
-fun ImagenHeroe(){
+fun ImagenHeroe(imageName: String){
+    val context = LocalContext.current
+    val ImageResId = remember (imageName){  context.resources.getIdentifier(imageName.lowercase(),
+        "drawable", context.packageName)
+    }
     Image(
-        painter = painterResource(R.drawable.logo_dragonballapi),
+        painter = painterResource(id = ImageResId),
         contentDescription ="Goku",
         modifier= Modifier
             .padding(16.dp)
